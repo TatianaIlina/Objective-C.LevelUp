7c8ab2fc51479301573afe56a86ef2320ca6f20c
Tatiana Ilina <fghjk51@mail.ru>
2013-08-25 07:23:41 +0400

    Initial Commit

---
+++ GitWelcome/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitWelcome
+//
+//  Created by Полярис on 25.08.13.
+//  Copyright (c) 2013 IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitWelcome/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitWelcome
+//
+//  Created by Полярис on 25.08.13.
+//  Copyright (c) 2013 IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
