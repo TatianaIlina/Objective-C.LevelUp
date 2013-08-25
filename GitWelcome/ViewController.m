0eb460eae2919b5cb47354150164c5a8fdb1f586
Tatiana Ilina <fghjk51@mail.ru>
2013-08-25 07:29:12 +0400

    Я X-coder!

--- GitWelcome/ViewController.m
+++ GitWelcome/ViewController.m
@@ -19,6 +19,9 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     // Hellow!
+    // I'm X-coder!
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
